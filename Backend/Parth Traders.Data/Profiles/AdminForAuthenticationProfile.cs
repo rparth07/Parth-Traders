978afb735a2c435ebb4ae7200dad39cfed97ab38
rparth07 <pmr642001@gmail.com>
2022-10-02 20:07:52 +0530

    created mapper profile for admin authentication

---
+++ Backend/Parth Traders.Data/Profiles/AdminForAuthenticationProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Parth_Traders.Data.DataModel.Admin;
+using Parth_Traders.Domain.Entity.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parth_Traders.Data.Profiles
+{
+    public class AdminForAuthenticationProfile : Profile
+    {
+        public AdminForAuthenticationProfile()
+        {
+            CreateMap<AdminForAuthentication, AdminForAuthenticationDataModel>();
+        }
+    }
+}
---
+++ Backend/Parth Traders/Profiles/AdminForAuthenticationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Parth_Traders.Domain.Entity.Admin;
+using Parth_Traders.Dto.Admin;
+
+namespace Parth_Traders.Profiles
+{
+    public class AdminForAuthenticationProfile : Profile
+    {
+        public AdminForAuthenticationProfile()
+        {
+            CreateMap<AdminForAuthenticationDto, AdminForAuthentication>();
+        }
+    }
+}
