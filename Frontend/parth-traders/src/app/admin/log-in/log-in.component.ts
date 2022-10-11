35df2350ce82a4b71d6228317e03a4e7b6bcf450
rparth07 <pmr642001@gmail.com>
2022-10-11 23:45:14 +0530

    refactored log-in component and init auth service

--- Frontend/parth-traders/src/app/admin/log-in/log-in.component.ts
+++ Frontend/parth-traders/src/app/admin/log-in/log-in.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.css']
+  styleUrls: ['./log-in.component.css'],
 })
 export class LogInComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login(form: NgForm) {
+    console.log('login');
   }
-
 }
