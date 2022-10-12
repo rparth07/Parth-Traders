7683f251602b85c350dce28320f5c02c38a5b1c9
rparth07 <pmr642001@gmail.com>
2022-10-13 00:00:07 +0530

    added auth service functionality

--- Frontend/parth-traders/src/app/admin/log-in/auth.service.ts
+++ Frontend/parth-traders/src/app/admin/log-in/auth.service.ts
@@ -1,8 +1,75 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, tap } from 'rxjs';
+import { throwError } from 'rxjs';
+import { TokenService } from './token.service';
 
+const API_URL = 'http://localhost:5031/';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  redirectUrl = '';
+
+  private static handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
+
+  private static log(message: string): any {
+    console.log(message);
+  }
+
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  login(loginData: any): Observable<any> {
+    this.tokenService.removeToken();
+    this.tokenService.removeRefreshToken();
+    const body = new HttpParams()
+      .set('username', loginData.username)
+      .set('password', loginData.password);
+
+    return this.http.post<any>(API_URL + 'oauth/token', body).pipe(
+      tap((res) => {
+        this.tokenService.saveToken(res.access_token);
+        this.tokenService.saveRefreshToken(res.refresh_token);
+      }),
+      catchError(AuthService.handleError)
+    );
+  }
+
+  refreshToken(refreshData: any): Observable<any> {
+    this.tokenService.removeToken();
+    this.tokenService.removeRefreshToken();
+    const body = new HttpParams().set(
+      'refresh_token',
+      refreshData.refresh_token
+    );
+
+    return this.http.post<any>(API_URL + 'oauth/token', body).pipe(
+      tap((res) => {
+        this.tokenService.saveToken(res.access_token);
+        this.tokenService.saveRefreshToken(res.refresh_token);
+      }),
+      catchError(AuthService.handleError)
+    );
+  }
+
+  logout(): void {
+    this.tokenService.removeToken();
+    this.tokenService.removeRefreshToken();
+  }
 }
