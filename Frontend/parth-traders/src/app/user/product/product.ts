ade10c4766b0e18166abcb3a3a1c5cc0e2c7f25a
rparth07 <pmr642001@gmail.com>
2022-12-17 23:13:57 +0530

    created product component

---
+++ Frontend/parth-traders/src/app/user/product/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  title: string;
+  price: number;
+  rating: number;
+  image: string;
+}
--- Frontend/parth-traders/src/app/user/user.module.ts
+++ Frontend/parth-traders/src/app/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './header/header.component';
+import { UserRoutingModule } from './user-routing.module';
+import { UserComponent } from './user.component';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [UserComponent, HeaderComponent, HomeComponent, ProductComponent],
+  imports: [CommonModule, MatIconModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
