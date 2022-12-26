e80bf7244c4aefae859ce53495742768a3196266
rparth07 <pmr642001@gmail.com>
2022-12-26 23:50:49 +0530

    added product list component

--- Frontend/parth-traders/src/app/user/user.module.ts
+++ Frontend/parth-traders/src/app/user/user.module.ts
@@ -6,9 +6,10 @@ import { UserComponent } from './user.component';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [UserComponent, HeaderComponent, HomeComponent, ProductComponent],
+  declarations: [UserComponent, HeaderComponent, HomeComponent, ProductComponent, ProductListComponent],
   imports: [CommonModule, MatIconModule, UserRoutingModule],
 })
 export class UserModule {}
